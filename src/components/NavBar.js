b97394e2ca1c2479340012af82c9bad0f737b3d2
LeonardoAlmds <leonardoalmd8@outlook.com>
2023-02-12 21:35:46 -0300

    Consertando Modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,12 @@
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import { Form } from 'react-bootstrap';
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import '../styles/NavBar.css';
 import EteLogo from '../images/ETE.png'
+import ModalLogin from './ModalLogin'
+import { Button } from 'react-bootstrap'
 
 const NavBar = () => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  /*
-  const [isActive, setIsActive] = useState(false)
-  const burguerClick = () => setIsActive(!isActive)
-  */
+  const [openModal, setOpenModal] = useState(true)
 
   return (
     <>
@@ -26,41 +18,13 @@ const NavBar = () => {
           <Link to='/horarioProfessores'>Horários dos Professores</Link> <hr/>
           <Link to='/horarioRefeicoes'>Horários das Refeições</Link> <hr/>
           <Link to='/cardapio'>Cardápio da Semana</Link> <hr/>
-          <Link to=''><Button variant="danger" className='buttonLogin' onClick={handleShow}>Login</Button></Link>
         </div>
+        <Button variant="danger" className='buttonLogin' onClick={ () => setOpenModal(true) }>
+          Login
+          <ModalLogin isOpen={openModal} />
+        </Button>
+        {console.log(openModal)}
       </nav>
-
-    <div>
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Login</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>E-mail:</Form.Label>
-                    <Form.Control type="email" placeholder="Insira seu email aqui..." />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Senha</Form.Label>
-                    <Form.Control type="password" placeholder="Insira sua senha..." />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Lembrar de mim" />
-                </Form.Group>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="danger" onClick={handleClose}>
-                Sair
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                Entrar
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    </div>
     </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import FormSelects from '../components/FormSelects';
 import TableHorarios from '../components/TableHorarios';
-import { getHorarioList } from '../services/horariosService';
 
 const Home = () => {
-  
-  getHorarioList();
-
   return (
     <>
       <FormSelects 
